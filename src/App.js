138deaade7a96698b88bd669efd3992bbe1e8c55
Antonio Aguila <aguilantonio16@gmail.com>
2020-10-12 17:58:22 -0500

    adding some initial basic components
    -Header component added
    -Footer component added
    -HomeScreen Product listing component added
    -Implementing react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./Screens/HomeScreen";
+import {ProductScreen} from "./Screens/ProductScreen";
 
 const App = () => {
   return (
     <>
-   <h1>hello world</h1>
+    <Router>
+    <Header/>
+      <main className="py-3">
+        <Container>
+          <Route exact path='/' component={HomeScreen} />
+          <Route exact path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+    <Footer/>
+    </Router>
    </>
   );
 }
