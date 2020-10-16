5c7635bfcd70a05028d2264bf1ccaffb135d0b6b
Antonio Aguila <aguilantonio16@gmail.com>
2020-10-16 17:54:34 -0500

    adding use Fetch hook in home

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,20 +1,39 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap';
 import { Product } from '../components/Product';
-import products from "../products";
+//import products from "../products";
+import { useFetch } from '../hooks/useFetch'
 
 const HomeScreen = () => {
+
+    const { data , loading } = useFetch(`http://localhost:5000/api/v1/products`)
+
     return (
         <>
 
-            <h1>latest products</h1>
-            <Row>
-                {products.map((product) => (
-                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                    <Product product={product} />
-                </Col>
-                ))}
-            </Row>
+            { loading
+                ?
+                ( 
+                    <h1 className="text-center">
+                    Loading ...
+                    </h1>
+                )
+                :
+                (   
+                <>
+                    <h1>latest products</h1>
+                    <Row>
+                        { data.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                        ))}
+                    </Row>
+                </>
+                )
+            }
+
+            
             
         </>
     )
