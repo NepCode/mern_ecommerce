a1d6c8a489528d5df15ff6742112f7589b8f2f36
Antonio Aguila <aguilantonio16@gmail.com>
2020-10-30 17:59:40 -0600

    user reducer and userTypes added

---
+++ src/reducers/userReducers.js
@@ -0,0 +1,40 @@
+
+import { types } from "../types/types";
+
+
+const initialState = {
+    
+}
+
+
+export const userLoginReducer = ( state = initialState , action ) => {
+
+    switch ( action.type ) {
+
+        case types.userTypes.USER_LOGIN_REQUEST:
+            return {
+                loading: true
+            }
+
+        case types.userTypes.USER_LOGIN_SUCCESS:
+            return {
+                userInfo: action.payload,
+                loading: false
+            }
+
+        case types.userTypes.USER_LOGIN_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case types.userTypes.USER_LOGIN_LOGOUT:
+            return {
+                userInfo: {}
+            }
+
+        default:
+            return state;
+    }
+
+}
