68c794234d7accee9f0dd263809ab90a3e996b61
Antonio Aguila <aguilantonio16@gmail.com>
2021-01-31 11:11:19 -0600

    adding ES modules, basic server api

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require('express');
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import products from './data/products.js'
+
+dotenv.config()
 
 const app = express();
 
@@ -7,14 +10,16 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 })
 
-app.get('/api/products', (req, res) => {
+app.get('/api/v1/products', (req, res) => {
     res.json(products)
 })
 
 
-app.get('/api/products/:id', (req, res) => {
+app.get('/api/v1/products/:id', (req, res) => {
     const product = products.find( product => product._id === req.params.id )
     res.json(product)
 })
 
-app.listen(5000, console.log('Server running on port 5000'));
+
+const PORT = process.env.PORT || 5000
+app.listen( PORT , console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT} ` ));
