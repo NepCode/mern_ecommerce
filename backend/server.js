74a1075d2366ed343efde3ecc06165731806d483
Antonio Aguila <aguilantonio16@gmail.com>
2021-02-27 23:03:57 -0600

    prepare  for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import products from './data/products.js'
 import connectDB from './config/db.js'
+import path from 'path'
 
 
 import productRoutes from './routes/productRoutes.js'
@@ -24,6 +25,21 @@ app.use('/api/v1/orders', orderRoutes)
 app.get('/api/v1/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen( PORT , console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT} ` ));
